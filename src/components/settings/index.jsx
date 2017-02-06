b3f6606d697fa5b7bccaa738d63d5fd38e8a0321
jamesmartin <jamesmartin442@gmail.com>
2017-02-06 09:08:36 +0000

    Also added settings

---
+++ src/components/settings/index.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {inject, observer} from 'mobx-react';
+import {Checkbox} from 'semantic-ui-react';
+import {map} from 'lodash';
+
+import './style.scss';
+
+@inject('settings')
+@observer
+class Index extends Component {
+
+	getSettingsList() {
+		return {
+			showEmptyMonths: {
+				label: 'Show Empty Months'
+			}
+		}
+	};
+
+	changeSetting = (e, data) => {
+		console.log(data);
+		this.props.settings.update(data.name, data.checked);
+	};
+
+	render() {
+
+		const {settings} = this.props;
+
+		if (this.props.settings.loaded === false) {
+			return <h2>Loading...</h2>;
+		}
+
+		let settingsList = this.getSettingsList();
+
+		settingsList = map(settingsList, (setting, name) => {
+			return (
+				<tr key={name}>
+					<td>{setting.label}</td>
+					<td><Checkbox toggle name={name} checked={settings[name]} onChange={this.changeSetting}/></td>
+				</tr>
+			)
+		});
+
+		return (
+			<div>
+				<h1>Settings</h1>
+				<table className="settings-list">
+					{settingsList}
+				</table>
+				{/*Show Empty Months: <Checkbox toggle checked={settings.showEmptyMonths} />*/}
+			</div>
+		);
+	}
+
+}
+
+export default Index;
---
+++ src/store/base/itemStore.js
@@ -0,0 +1,31 @@
+import moment from 'moment';
+
+class ItemStore {
+
+	addOrReplaceInList(items) {
+
+		if (!this.items.length) {
+			return items;
+		}
+
+		const itemIds = items.map(i => i.id);
+
+		const filteredItems = this.items.filter((item) => {
+			return !itemIds.includes(item.id);
+		});
+
+		return filteredItems.concat(items);
+
+	}
+
+	sortList(items) {
+		return items.sort((a, b) => {
+			const aDate = moment(a.startDate);
+			const bDate = moment(b.startDate);
+
+			return aDate.diff(bDate);
+		});
+	}
+
+}
+export default ItemStore;
