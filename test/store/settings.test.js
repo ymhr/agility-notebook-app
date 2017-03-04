2de160794468113ec1eedf86e1146a4587e18a12
jamesmartin <jamesmartin442@gmail.com>
2017-03-04 16:45:40 +0000

    I think I fixed the tests!

--- test/store/settings.test.js
+++ test/store/settings.test.js
@@ -3,40 +3,50 @@ import should from 'should';
 import {afterEach, beforeEach, describe, it} from "mocha";
 import auth from "../../src/store/auth";
 import Settings from '../../src/store/settings';
+import {stub} from 'sinon';
+
 require('should-sinon');
 
-beforeEach(function(){
-	stub(auth, 'get').callsFake(function(){
-		return new Promise(function(resolve){
-			resolve({
-				data: [
-					{id: 1, name: 'Pepe'},
-					{id: 2, name: 'Toby'}
-				]
-			});
-		});
-	});
+describe('settings', function(){
+
+    let authStub;
+
+    beforeEach(function(){
+    	authStub = stub(auth, 'get').callsFake(function(){
+    		return new Promise(function(resolve){
+    			resolve({
+    				data: [
+    					{id: 1, name: 'Pepe'},
+    					{id: 2, name: 'Toby'}
+    				]
+    			});
+    		});
+    	});
+    });
+
+
+
+    afterEach(function(){
+    	Settings.showEmptyMonths = undefined;
+    	authStub.restore();
+    });
+
+    describe('applying a setting', function(){
+
+    	it('should be able to set showEmptyMonths and have 1 converted to true', function(){
+
+    	});
+
+    	it('should be able to set showEmptyMonths and have 0 converted to false', function(){
+
+    	});
+
+    });
+
+    describe('updating a setting', function(){});
+
+    describe('loading the settings', function(){});
+
+    describe('settings defaults', function(){});
+
 });
-
-afterEach(function(){
-	Settings.showEmptyMonths = undefined;
-	auth.get.restore()
-});
-
-describe('applying a setting', function(){
-
-	it('should be able to set showEmptyMonths and have 1 converted to true', function(){
-
-	});
-
-	it('should be able to set showEmptyMonths and have 0 converted to false', function(){
-
-	});
-
-});
-
-describe('updating a setting', function(){});
-
-describe('loading the settings', function(){});
-
-describe('settings defaults', function(){});
