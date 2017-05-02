7c27d0d199d9ae19b52dbe5f70c5cc331a0ccd71
James Martin <jamesmartin442@gmail.com>
2017-05-02 13:59:20 +0100

    Re-added sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
 	 	publicPath: '/',
 	 	inline: true
  	},
-	//devtool: 'eval-source-map',
+	devtool: 'eval-source-map',
 	plugins: [
 		// new DashboardPlugin(),
 		new CommonsChunkPlugin({
