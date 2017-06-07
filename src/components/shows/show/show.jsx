c9a0bcba4a93fb15cb98bef565c8c1ac2a6b4a11
jamesmartin <jamesmartin442@gmail.com>
2017-06-07 20:28:26 +0100

    Added expand panels for shows

--- src/components/shows/show/show.jsx
+++ src/components/shows/show/show.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
-import {List} from 'semantic-ui-react';
+import {List, Button, Label, Icon} from 'semantic-ui-react';
 import {hashHistory} from 'react-router';
 import moment from 'moment';
 
@@ -8,9 +8,54 @@ import './style.css';
 
 @observer
 class Show extends Component {
+	
+	constructor(){
+		super();
+		this.state = {expanded: false};
+	}
 
 	itemClick = (show) => {
 		hashHistory.push(`/shows/${show.id}`);
+	};
+
+	toggleExpanded = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		console.log('called');
+		this.setState({expanded: !this.state.expanded});
+	};
+
+	arrowButton = () => {
+		const props = {
+			onClick: this.toggleExpanded
+		};
+		return (
+			this.state.expanded ?
+				<Button circular icon="angle up" {...props} /> :
+				<Button circular icon="angle down" {...props} />
+		);
+	};
+
+	expandedDetails = () => {
+
+		if(!this.state.expanded) return '';
+
+		const {show} = this.props;
+		return (
+			<div>
+				<Label className="showDetails">Runs <Label.Detail>{show.runs.length}</Label.Detail></Label>
+				<Label className="showDetails">Booking platform <Label.Detail>{show.bookingPlatform}</Label.Detail></Label>
+				<Label className="showDetails">Booked <Label.Detail>{show.bookedIn ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Paid <Label.Detail>{show.paid ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Hotel needed <Label.Detail>{show.hotelNeeded ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Hotel booked <Label.Detail>{show.hotelBooked ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Holiday needed <Label.Detail>{show.holidayNeeded ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Holiday booked <Label.Detail>{show.holidayBooked ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Camping needed <Label.Detail>{show.campingRequired ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Camping booked <Label.Detail>{show.campingBooked ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+				<Label className="showDetails">Camping confirmed <Label.Detail>{show.campingConfirmed ? <Icon name="check" /> : <Icon name="close" />}</Label.Detail></Label>
+			</div>
+		);
 	};
 
 	render() {
@@ -26,12 +71,14 @@ class Show extends Component {
 		return (
 
 			<List.Item onClick={this.itemClick.bind(this, show)} className={classNames.join(' ')}>
-				<List.Content floated="right">Runs: {show.runs.length}</List.Content>
+				{/*<List.Content floated="right">Runs: {show.runs.length}</List.Content>*/}
+				<List.Content floated="right">{this.arrowButton()}</List.Content>
 				<List.Content>
 					<List.Header>{show.name}</List.Header>
 					<List.Description>
 						<div>{moment(show.startDate).format('dddd, MMMM Do YYYY')} - {moment(show.endDate).format('dddd, MMMM Do YYYY')}</div>
 						{closingSoon}
+						{this.expandedDetails()}
 					</List.Description>
 				</List.Content>
 			</List.Item>
--- webpack.config.js
+++ webpack.config.js
@@ -35,10 +35,6 @@ module.exports = {
 			}
 		},
 		{
-			test: /\.scss$/,
-			use: ['style-loader', 'css-loader', 'sass-loader']
-		},
-		{
 			test: /\.css$/,
 			use: [
 				'style-loader',
