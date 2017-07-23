d6c6bd923a5c8739bc88d50be8f1eea0e36f65c9
jamesmartin <jamesmartin442@gmail.com>
2017-07-23 15:13:04 +0100

    Fixed logo for login button

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -14,7 +14,7 @@ class Login extends Component {
                 <Container textAlign="center">
                     <br /><br /><br />
                     <Header as="h1">You must log in to use this site.</Header>
-                    <Button onClick={this.redirectToLogin} primary icon><Icon name="Facebook" />Login with Facebook</Button>
+                    <Button onClick={this.redirectToLogin} primary icon><Icon name="facebook" />Login with Facebook</Button>
                 </Container>
         );
 
