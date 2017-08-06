dd42c9aab5237b91c26c32e8d37910fe65668caa
jamesmartin <jamesmartin442@gmail.com>
2017-08-06 18:55:00 +0100

    Moved the logout button

--- src/components/root/header.jsx
+++ src/components/root/header.jsx
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import routes from '../routes';
 import {Header, Menu, Button, Container} from 'semantic-ui-react';
 import {hashHistory} from 'react-router';
-import {observer} from 'mobx-react';
+import {observer, inject} from 'mobx-react';
 
 import styles from './styles.css';
 
-@observer(['auth'])
+@inject('auth')
+@observer
 class HeaderContainer extends Component {
 
     clickMenuItem = (path) => {
@@ -29,7 +30,8 @@ class HeaderContainer extends Component {
 
         menu = [
             <Menu.Item key={Math.random()} onClick={this.clickMenuItem.bind(this, routes[0].path)}>{routes[0].label}</Menu.Item>,
-                ...menu
+                ...menu,
+            <Menu.Item key={Math.random()} onClick={this.logout} position="right">Logout</Menu.Item>
         ];
 
         const navStyles = {
@@ -47,7 +49,6 @@ class HeaderContainer extends Component {
                             <Header.Subheader>Makes notes and stuff</Header.Subheader>
                         </Header>
                     </Container>
-                    <Button size="mini" floated="right" onClick={this.logout}>Logout</Button>
 
                 </header>
 
